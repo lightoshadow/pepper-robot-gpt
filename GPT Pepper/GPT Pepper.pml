0bc3a898ab49ce883dd525256270c2691a79dc7b
LightOfShadow <102809422+lightoshadow@users.noreply.github.com>
2024-05-30 11:44:52 +0200

    Add files via upload

---
+++ GPT Pepper/GPT Pepper.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="GPT EMMA" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="epicsax" src="behavior_1/epicsax.ogg" />
+        <File name="images" src="html/image.jpg" />
+        <File name="pippo" src="html/pippo.jpg" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ GPT Pepper/translations/translation_en_US.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>benvenuti nella nostra scuola,. vi faccio vedere tutte le cose che sappiamo fare.. seguitemi.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">benvenuti nella nostra scuola,. vi faccio vedere tutte le cose che sappiamo fare.. seguitemi.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>non sono molto veloce ma la giù stanno aspettando proprio voi... prego continuate</source>
+            <comment>Text</comment>
+            <translation type="unfinished">non sono molto veloce ma la giù stanno aspettando proprio voi... prego continuate</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>batti il pugno</source>
+            <comment>Text</comment>
+            <translation type="unfinished">batti il pugno</translation>
+        </message>
+    </context>
+</TS>
